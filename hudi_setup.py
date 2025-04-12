be8e8f71f4c76fed9770bfc0759d7c2673f7dcd3
SimonH <professional.simonhossain@gmail.com>
2025-04-12 23:16:35 +0100

    spark session builder

--- hudi_setup.py
+++ hudi_setup.py
@@ -1,5 +1,15 @@
 from pyspark.shell import spark
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, col
+
+# Initialize Spark session
+spark = SparkSession.builder \
+    .appName("HudiExample") \
+    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+    .config("spark.sql.hive.convertMetastoreParquet", "false") \
+    .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtensions") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.hudi.catalog.HoodieCatalog") \
+
 
 
 columns = ["timestamp","uuid","rider_id","driver_id","fare","city"]
@@ -17,7 +27,7 @@ df = spark.createDataFrame(data, columns)
 # Hudi table options
 hudi_options = {
     "hoodie.table.name": "hudi_example_table",
-    "hoodie.datasource.write.recordkey.field": "id",
+    "hoodie.datasource.write.recordkey.field": "uuid",
     "hoodie.datasource.write.precombine.field": "timestamp",
     "hoodie.datasource.write.table.type": "COPY_ON_WRITE",
     "hoodie.datasource.write.operation": "upsert",
@@ -33,16 +43,17 @@ df.write.format("hudi") \
     .mode("overwrite") \
     .save("file:///tmp/hudi_example_table")
 
-def insert_data_to_table(table_name, data_frame):
-    """
-    Inserts data into the specified table in the database.
-    """
-    # Check if the table exists
-    if spark._jsparkSession.catalog().tableExists(table_name):
-        # Append data to the existing table
-        data_frame.write.format("delta").mode("append").saveAsTable(table_name)
-    else:
-        # Create a new table and insert data
-        data_frame.write.format("delta").mode("overwrite").saveAsTable(table_name)
-
-
+# def insert_data_to_table(table_name, data_frame):
+#     """
+#     Inserts data into the specified table in the database.
+#     """
+#     # Check if the table exists
+#     if spark._jsparkSession.catalog().tableExists(table_name):
+#         # Append data to the existing table
+#         data_frame.write.format("delta").mode("append").saveAsTable(table_name)
+#     else:
+#         # Create a new table and insert data
+#         data_frame.write.format("delta").mode("overwrite").saveAsTable(table_name)
+#
+# # Insert data into the Hudi table
+# insert_data_to_table("hudi_example_table", df)
